3e503909ca2f7005bc42670f640d16e5e7100ad2
Sergio Osuna <sergio.osuna@MAC-sergioosuna.local>
2019-09-27 08:06:39 +0200

    Generate SSH key for Flux to use, avoid having to enable Flux's autogenerated one in the repos each time.

--- demo-0-setup.sh
+++ demo-0-setup.sh
@@ -5,5 +5,11 @@ minikube start -p flux-demo --memory 8192
 # See kube internal resources
 kubectl get all --all-namespaces 
 
+# Create ssh key for Flux
+ssh-keygen -t rsa -b 4096 -f flux-git-ssh-key -N '' -C flux-git-ssh-key
+
+
+# Undo
+
 # Destroy minikube
 minikube delete -p flux-demo
