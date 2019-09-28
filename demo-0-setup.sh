dcc98f62397334db1719072d549a07f07031123d
Sergio Osuna <sergio.osuna@MAC-sergioosuna.local>
2019-09-28 20:20:03 +0200

    Use official documentation

--- demo-0-setup.sh
+++ demo-0-setup.sh
@@ -6,7 +6,7 @@ minikube start -p flux-demo --memory 8192
 kubectl get all --all-namespaces 
 
 # Create ssh key for Flux
-# https://github.com/fluxcd/flux/blob/1.12.2/test/e2e/e2e-git.sh#L11-L12
+# https://docs.fluxcd.io/en/stable/guides/provide-own-ssh-key.html
 ssh-keygen -t rsa -b 4096 -f flux-git-ssh-key -N '' -C flux-git-ssh-key
 
 
